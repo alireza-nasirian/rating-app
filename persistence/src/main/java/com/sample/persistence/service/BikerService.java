5760dd9108d6f95008abd5d1344d6fc6d52db49a
alireza-nasirian <an1380an@gmail.com>
2023-10-28 05:43:44 +0330

    [enhancement] fix typo

--- persistence/src/main/java/com/sample/persistence/service/BikerService.java
+++ persistence/src/main/java/com/sample/persistence/service/BikerService.java
@@ -3,5 +3,5 @@ package com.sample.persistence.service;
 public interface BikerService {
 
     Double getBikerRateById_Top_Bottom (Long id);
-    Double getBikerRateById_withOutOutlayers (Long id);
+    Double getBikerRateById_withOutOutliers(Long id);
 }
