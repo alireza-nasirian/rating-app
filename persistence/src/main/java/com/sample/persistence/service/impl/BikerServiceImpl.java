95c12638ca43d225bc95eba0483953822bd82b34
alireza-nasirian <an1380an@gmail.com>
2023-10-28 05:12:15 +0330

    [enhancement] enhance biker scoring system

--- persistence/src/main/java/com/sample/persistence/service/impl/BikerServiceImpl.java
+++ persistence/src/main/java/com/sample/persistence/service/impl/BikerServiceImpl.java
@@ -4,9 +4,13 @@ import com.sample.common.model.general.Biker;
 import com.sample.persistence.converter.PersistenceMapper;
 import com.sample.persistence.dao.BikerDAO;
 import com.sample.persistence.entity.BikerEntity;
+import com.sample.persistence.entity.FeedbackEntity;
+import com.sample.persistence.entity.RateEntity;
 import com.sample.persistence.service.BikerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -18,7 +22,17 @@ public class BikerServiceImpl implements BikerService {
     @Override
     public Biker getBikerById(Long id) {
         BikerEntity bikerEntity = bikerDAO.getReferenceById(id);
-        Double averageRate = bikerEntity.getFeedbacks().stream().mapToInt(a -> a.getRate().getValue()).average().orElse(0.0);
+        List<FeedbackEntity> feedbacks = bikerEntity.getFeedbacks();
+        double averageRate = 0;
+        if (feedbacks.isEmpty()) {
+            long top = bikerEntity.getFeedbacks().stream()
+                    .filter(a -> a.getRate() == RateEntity.FIVE_STAR || a.getRate() == RateEntity.FOUR_STAR).count();
+            long bottom = bikerEntity.getFeedbacks().stream()
+                    .filter(a -> a.getRate() == RateEntity.ONE_STAR || a.getRate() == RateEntity.TWO_STAR ||
+                            a.getRate() == RateEntity.FOUR_STAR || a.getRate() == RateEntity.FIVE_STAR).count();
+
+            averageRate = (double) ((top/bottom) * 5);
+        }
         Biker biker = mapper.fromBikerEntity(bikerEntity);
         biker.setAverageRate(averageRate);
         return biker;
